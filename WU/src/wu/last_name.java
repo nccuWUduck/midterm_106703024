380771e63c4c95678220fb7820cdc12da0c399ce
106703024 <@Fy3302>
2019-12-31 11:24:11 +0800

    WU

--- WU/src/wu/last_name.java
+++ WU/src/wu/last_name.java
@@ -4,7 +4,7 @@ public class last_name {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" Hello");
+		System.out.println(" Hello WU");
 	}
 
 }
